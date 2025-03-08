572b119eb9f4fd16ac02bddad278b761ce98cbba
julian190 <68065877+julian190@users.noreply.github.com>
2025-03-08 15:59:24 +1300

    fixes

--- src/app/api/category/[id]/route.ts
+++ src/app/api/category/[id]/route.ts
@@ -72,7 +72,8 @@ import prisma from '@/lib/prisma'
 //   }
 // }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+  const { params } = context;
   const session = await getServerSession(authOptions)
   if (!session?.user?.email) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
